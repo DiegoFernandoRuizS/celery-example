c970996fbee1abc3fe9ce1b5565c7aea7693b36e
Diego F. Ruiz S <yego23@gmail.com>
2016-09-01 14:09:32 -0500

    Update convert to video - celery process

--- diego/web/tasks.py
+++ diego/web/tasks.py
@@ -1,10 +1,24 @@
 from __future__ import absolute_import
 
+import subprocess
 from celery import task
+
 
 @task
 def test(param):
-    return 'Listo esta es la tarea y el parametro es:  "%s" ' % param
+    pathConverted = 'video\\convertido.mp4'
+    path = 'video\\video3.avi'
+
+    print('Ejecutando conversion ...')
+
+    resultado = subprocess.call('ffmpeg -i '+path+'  -b 1500k -vcodec libx264 -g 30 '+pathConverted)
+
+    if resultado != 0:
+        print('Algo falló en la conversión del video %s', resultado)
+    else:
+        print('Video convertido ok')
+
+    return 'La tarea de conversion de video esta ok. es:  "%s" ' % resultado
 
 
 @task
